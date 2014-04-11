855913dbbc15ae702bcfe4b26ed7c7087ad926c6
AWright789 <adam.wright@synechron.com>
2014-04-11 16:58:07 -0500

    Ver3
    test

---
+++ AuctionLand.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuctionLand.Web.Startup))]
+namespace AuctionLand.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
