3ccc354946d2862720482300a5d50d85d01dcadc
athilavong <andy.thilavong@gmail.com>
2014-04-24 14:34:10 -0700

    Updated Admin

--- AuctionLand.Web/Models/RealEstateModel.cs
+++ AuctionLand.Web/Models/RealEstateModel.cs
@@ -53,10 +53,69 @@ namespace AuctionLand.Web.Models
         public double EndingBid { get; set; }
         public double BidIncrement { get; set; }
 
+
+        public string DefaultImageUrl
+        {
+            get
+            {
+                if (this.RealEstateImageModels.FirstOrDefault() != null)
+                {
+                    return this.RealEstateImageModels.FirstOrDefault().ImageUrl ?? string.Empty;
+                }
+                return string.Empty;
+            }
+            set
+            {
+
+            }
+        }
         public DateTime? SaleDate { get; set; }
 
         public IEnumerable<RealEstateImageModel> RealEstateImageModels { get; set; }
+        public ListingStatus ListingStatus
+        {
+            get
+            {
+                return (ListingStatus)ListingStatusId;
+            }
+            set
+            {
+                ListingStatusId = (int)value;
+            }
+        }
 
-
+        public ListingType? ListingType
+        {
+            get
+            {
+                return (ListingType)ListingTypeId;
+            }
+            set
+            {
+                ListingTypeId = (int)value;
+            }
+        }
+        public OccupancyStatus? OccupancyStatus
+        {
+            get
+            {
+                return (OccupancyStatus)OccupancyStatusId;
+            }
+            set
+            {
+                OccupancyStatusId = (int)value;
+            }
+        }
+        public RealEstateType RealEstateType
+        {
+            get
+            {
+                return (RealEstateType)RealEstateTypeId;
+            }
+            set
+            {
+                RealEstateTypeId = (int)value;
+            }
+        }
     }
 }
